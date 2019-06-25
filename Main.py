5240b89e8e225d12ed8e05216661e18a61f0f60e
Miksail <25419235+Miksail@users.noreply.github.com>
2019-06-25 03:03:37 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from AnbuBot import Anbu
 
 if __name__ == "__main__":
-    TOKEN = "NTI5MDA3ODU0NjUzNDA3MjMz.XLVipA.AfQML1GC4JvAehpkeuO55TA1JUE"
+    TOKEN = "HERE YOUR TOKEN"
     bot = Anbu()
     bot.run(TOKEN)
 
